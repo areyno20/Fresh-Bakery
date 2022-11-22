44f42f820dcb49a057bdb6c12dc412f07fd8562f
Tonyoffor <tonyc.offor@gmail.com>
2022-11-22 18:44:18 -0500

    fixed the weather call at the bottom

--- assets/js/script.js
+++ assets/js/script.js
@@ -13,22 +13,23 @@ const totalQuantity = [quantity1*30 + quantity2*15 + product3*10 + product4*5]
 function orderInput(){
 
 }
-let map = initMap();
+
 
 //Displays the location of the store at the bottom
-// function initMap(){
-//   document.getElementById("map").display
-//     var location = {lat: 43.659815, lng: -79.408903}
-//     var map = new google.maps.Map(document.getElementById("map"),{
-//       zoom: 4,
-//       center: location
-//     });
-//     var marker = new google.maps.Marker({
-//       position: location,
-//       map: map
-//     });
-//   }
-
+function initMap(){
+  document.getElementById("map").display
+    var location = {lat: 43.659815, lng: -79.408903}
+    var map = new google.maps.Map(document.getElementById("map"),{
+      zoom: 4,
+      center: location
+    });
+    var marker = new google.maps.Marker({
+      position: location,
+      map: map
+    });
+    
+  }
+  initMap();
 
   function distancebetween(){
     //calculation between the initmap and user location input
