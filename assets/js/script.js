40b10076147d97b9c39e10a3af4e02117a76ae8a
MarioT573 <mariotrig@yahoo.ca>
2022-11-22 17:56:08 -0500

    google maps travel time implementation

--- assets/js/script.js
+++ assets/js/script.js
@@ -14,3 +14,41 @@ function orderInput(){
 }
 
 // delivery and pickup check boxes only one checkbox allowed
+
+var origin2 = 'St Patrick Station, Toronto, Canada';
+var destinationA = 'Finch Station, Toronto, Canada';
+
+var service = new google.maps.DistanceMatrixService();
+
+service.getDistanceMatrix(
+  {
+    origins: [origin2],
+    destinations: [destinationA],
+    travelMode: 'DRIVING',
+    drivingOptions: {
+        departureTime: new Date(Date.now()),
+        trafficModel: 'bestguess'
+    },
+    unitSystem: google.maps.UnitSystem.METRIC,
+    avoidHighways: true,
+    avoidTolls: true,
+  }, callback);
+
+function callback(response, status) {
+  if (status == 'OK') {
+    var origins = response.originAddresses;
+    var destinations = response.destinationAddresses;
+    for (var i = 0; i < origins.length; i++) {
+      var results = response.rows[i].elements;
+      for (var j = 0; j < results.length; j++) {
+        var element = results[j];
+        var distance = element.distance.text;
+        var duration = element.duration.text;
+        var from = origins[i];
+        var to = destinations[j];
+      }
+    }
+  }
+  $('#travelTime').text(`Estimated Travel Time: ${duration}`);
+  console.log(duration);
+}
