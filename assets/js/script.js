a1bf58284999c97de898f1000cf1b35d9df5e39f
Aidan <aidanreynolds715@gmail.com>
2022-11-22 19:17:18 -0500

    JS additions

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,7 @@
+// Total Order
 var orderNumber= document.getElementById('quantity') ; 
 var totalOrder= document.getElementById('totalorder') ; 
+
 //totalOrder.addEventListener("click", orderInput);
 
 var quantity1 = document.getElementById('quantity1')
@@ -14,3 +16,21 @@ function orderInput(){
 }
 
 // delivery and pickup check boxes only one checkbox allowed
+// $("input:checkbox").on('click', function() {
+//     var box= $(this);
+//     if (box.is(":checked")) {
+//       var group = "input:checkbox[name='" + box.attr("name") + "']";
+//       $(group).prop("checked", false);
+//       box.prop("checked", true);
+//     } else {
+//       box.prop("checked", false);
+//     }
+//   });
+
+// Order Form Auto Complete
+var autocomplete = google.maps.places.Autocomplete;
+var nameEl = document.querySelector("#name")
+var streetEl= document.querySelector("#street-address") 
+var cityEl = document.querySelector("#postal-code")
+var postalEl = document.querySelector("#city")
+var countryEl = document.querySelector("#country")
