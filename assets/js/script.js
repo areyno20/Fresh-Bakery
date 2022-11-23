7d9c43b824a28efadca6327a63351e9cef7cb287
MarioT573 <mariotrig@yahoo.ca>
2022-11-23 00:07:19 -0500

    product/form tailwind style and api connection with local storage

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,50 +1,34 @@
 $(function(){
-// Total Order
-var orderNumber= document.getElementById('quantity') ; 
-var totalOrder= document.getElementById('totalorder') ; 
+// // Total Order
+// var orderNumber= document.getElementById('quantity') ; 
+// var totalOrder= document.getElementById('totalorder') ; 
 
-//totalOrder.addEventListener("click", orderInput);
+// //totalOrder.addEventListener("click", orderInput);
 
-var quantity1 = document.getElementById('quantity1');
-var quantity2 = document.getElementById('quantity2');
-var quantity3 = document.getElementById('quantity3');
-var quantity4 = document.getElementById('quantity4');
+// var quantity1 = document.getElementById('quantity1');
+// var quantity2 = document.getElementById('quantity2');
+// var quantity3 = document.getElementById('quantity3');
+// var quantity4 = document.getElementById('quantity4');
 
-const totalQuantity = [quantity1*30 + quantity2*15 + product3*10 + product4*5]
+// const totalQuantity = [quantity1*30 + quantity2*15 + product3*10 + product4*5]
 
 function orderInput(){
 
 }
 
-//Displays the location of the store at the bottom
-function initMap(){
-  document.getElementById("map").display
-    var location = {lat: 43.659815, lng: -79.408903}
-    var map = new google.maps.Map(document.getElementById("map"),{
-      zoom: 4,
-      center: location
-    });
-    var marker = new google.maps.Marker({
-      position: location,
-      map: map
-    });
-    
-  }
-  initMap();
-
 function distancebetween(){
   //calculation between the initmap and user location input
 }
 
-var origin2 = 'St Patrick Station, Toronto, Canada';
-var destinationA = 'Finch Station, Toronto, Canada';
+var origin = `${localStorage.getItem("street-address")}, ${localStorage.getItem("city")}, ${localStorage.getItem("region")}`;
+var destination = 'Finch Station, Toronto, Ontario';
 
 var service = new google.maps.DistanceMatrixService();
 
 service.getDistanceMatrix(
   {
-    origins: [origin2],
-    destinations: [destinationA],
+    origins: [origin],
+    destinations: [destination],
     travelMode: 'DRIVING',
     drivingOptions: {
         departureTime: new Date(Date.now()),
@@ -70,7 +54,7 @@ function callback(response, status) {
       }
     }
   }
-  $('#travelTime').text(`Estimated Travel Time: ${duration}`);
+  $('#travelTime').text(`Estimated Travel Time from ${origin}: ${duration}`);
   console.log(duration);
 }
 
@@ -81,39 +65,82 @@ function callback(response, status) {
 
 const checkoutButton = document.getElementById("checkout");
 
-function storeData() {
+checkoutButton.addEventListener("click", function(event){
+  event.preventDefault();
 
-  var nameStorage = document.getElementById("name");
+  var firstnameStorage = document.getElementById("first-name");
+  var lastnameStorage = document.getElementById("last-name");
+  var emailAddressStorage = document.getElementById("email-address");
   var streetAddressStorage = document.getElementById("street-address");
+  var countryStorage = document.getElementById("country");
   var cityStorage = document.getElementById("city");
+  var provinceStorage = document.getElementById("region");
   var postalCodeStorage = document.getElementById("postal-code");
 
-  localStorage.setItem("name", nameStorage.value);
+  localStorage.setItem("first-name", firstnameStorage.value);
+  localStorage.setItem("last-name", lastnameStorage.value);
+  localStorage.setItem("email-address", emailAddressStorage.value);
+  localStorage.setItem("country", countryStorage.value);
   localStorage.setItem("street-address", streetAddressStorage.value);
   localStorage.setItem("city", cityStorage.value);
+  localStorage.setItem("region", provinceStorage.value);
   localStorage.setItem("postal-code", postalCodeStorage.value);
-};
 
 window.onload = function renderLastRegistered() {
-    var nameEl = document.getElementById("name")
+    var firstnameEl = document.getElementById("first-name")
+    var lastnameEl = document.getElementById("last-name")
+    var emailAddressEl = document.getElementById("email-address")
+    var countryEl = document.getElementById("country")
     var streetEl= document.getElementById("street-address") 
     var cityEl = document.getElementById("city")
+    var provinceEl = document.getElementById("region")
     var postalEl = document.getElementById("postal-code")
 
-    var name = localStorage.getItem("name");
-    nameEl.textContent = name;
+    var firstName = localStorage.getItem("first-name");
+    firstnameEl.textContent = firstName;
+
+    var lastName = localStorage.getItem("last-name");
+    lastnameEl.textContent = lastName;
+
+    var emailAddress = localStorage.getItem("email-address");
+    emailAddressEl.textContent = emailAddress;
+
+    var country = localStorage.getItem("country");
+    countryEl.textContent = country;
 
     var streetAddress = localStorage.getItem("street-address");
     streetEl.textContent = streetAddress;
 
     var city = localStorage.getItem("city");
     cityEl.textContent = city;
+
+    var province = localStorage.getItem("region");
+    provinceEl.textContent = province;
     
     var postalCode = localStorage.getItem("postal-code");
     postalEl.textContent = postalCode;
 
 }
+});
 
+
+//Displays the location of the store at the bottom
+function initMap(){
+  document.getElementById("map").display
+    var location = {lat: 43.659815, lng: -79.408903}
+    var map = new google.maps.Map(document.getElementById("map"),{
+      zoom: 4,
+      center: location
+    });
+    var marker = new google.maps.Marker({
+      position: location,
+      map: map
+    });
+    
+  }
+  initMap();
+
+});
 
 
 
